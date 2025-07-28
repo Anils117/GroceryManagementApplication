e27d9b8ec3e5a412db35f32ffadd100756cdf44c
Anil Kumar Mallem <83568202+Anils117@users.noreply.github.com>
2025-07-28 20:21:27 +0530

    Delete GroceryManagementApplication directory

--- GroceryManagementApplication/src/main/java/com/anil/gma/dao/ProductDao.java
+++
@@ -1,8 +0,0 @@
-package com.anil.gma.dao;
-
-import com.anil.gma.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductDao extends JpaRepository<Product,Integer> {
-
-}
